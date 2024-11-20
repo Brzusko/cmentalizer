595edb89b496db8b51a00375f0d1f62f84b36e2c
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-11-21 00:34:58 +0100

    Discarded network creator

--- rust-src/src/network/entrypoint/mod.rs
+++ rust-src/src/network/entrypoint/mod.rs
@@ -1,4 +1,1 @@
 pub(crate) mod network_entry_point;
-pub(crate) mod enet_host;
-pub(crate) mod utils;
-mod enet_client;
--- rust-src/src/network/entrypoint/network_entry_point.rs
+++ rust-src/src/network/entrypoint/network_entry_point.rs
@@ -1,53 +1,12 @@
 use godot::prelude::*;
 use godot::classes::{INode, Node};
-use crate::network::entrypoint::enet_client::{ENetClientArgs, ENetClient};
-use crate::network::entrypoint::enet_host::{ENetHostArgs, ENetHost};
 
-pub(crate) trait NetworkConstructor<T>
-where
-    T: Inherits<RefCounted>
-{
-    fn construct(&mut self, args: Gd<T>) -> bool;
-}
-
-pub(crate) trait NetworkConstructConsumer<T>
-where
-    T: Inherits<RefCounted>
-{
-    fn consume_args(&mut self, args: Gd<T>) -> bool;
-}
-
-pub(crate) trait CommonNetworkInterface
-{
-    fn is_initialized(&self) -> bool;
-}
-
-#[derive(GodotConvert, Var, Export)]
-#[godot(via = i64)]
-pub(crate) enum NetworkTransport
-{
-    None,
-    ENetHost,
-    ENetClient,
-    //SteamHost
-    //SteamClient
-}
-
-impl Default for NetworkTransport
-{
-    fn default() -> Self
-    {
-        NetworkTransport::None
-    }
-}
 
 #[derive(GodotClass)]
 #[class(base = Node, init)]
 pub(crate) struct NetworkEntryPoint
 {
     base: Base<Node>,
-    network_node: Option<Gd<Node>>,
-    network_transport_type: NetworkTransport,
 }
 
 #[godot_api]
@@ -58,93 +17,4 @@ impl INode for NetworkEntryPoint
 #[godot_api]
 impl NetworkEntryPoint
 {
-    #[func]
-    pub fn is_network_created(&self) -> bool
-    {
-        if self.network_node.is_none()
-        {
-            return false;
-        }
-
-        let network_node_ptr = self.network_node.as_ref().unwrap().clone();
-
-        match self.network_transport_type
-        {
-            NetworkTransport::None => false,
-            NetworkTransport::ENetHost => {
-                network_node_ptr
-                    .try_cast::<ENetHost>()
-                    .is_ok_and(|node| node.bind().is_initialized())
-            },
-            NetworkTransport::ENetClient => {
-                network_node_ptr
-                    .try_cast::<ENetClient>()
-                    .is_ok_and(|node| node.bind().is_initialized())
-            },
-        }
-    }
-
-    #[func]
-    pub fn create_enet_host(&mut self, args: Gd<ENetHostArgs>)
-    {
-        self.construct(args);
-    }
-
-    #[func]
-    pub fn create_enet_client(&mut self, args: Gd<ENetClientArgs>)
-    {
-        self.construct(args);
-    }
-}
-
-impl NetworkConstructor<ENetHostArgs> for NetworkEntryPoint
-{
-    fn construct(&mut self, args: Gd<ENetHostArgs>) -> bool
-    {
-        let mut enet_host_node = ENetHost::new_alloc();
-        self.base_mut().add_child(&enet_host_node);
-        enet_host_node.set_name(GString::from("Network"));
-        let mut construct_result= false;
-
-        {
-            construct_result = enet_host_node.bind_mut().consume_args(args);
-        }
-
-        if !construct_result
-        {
-            self.base_mut().remove_child(&enet_host_node);
-            enet_host_node.queue_free();
-            return false;
-        }
-
-        self.network_node = Some(enet_host_node.upcast());
-        self.network_transport_type = NetworkTransport::ENetHost;
-        return true;
-    }
-}
-
-impl NetworkConstructor<ENetClientArgs> for NetworkEntryPoint
-{
-    fn construct(&mut self, args: Gd<ENetClientArgs>) -> bool
-    {
-        let mut enet_client_node = ENetClient::new_alloc();
-        self.base_mut().add_child(&enet_client_node);
-        enet_client_node.set_name(GString::from("Network"));
-
-        let is_created;
-        {
-            let mut enet_client_bound = enet_client_node.bind_mut();
-            is_created = enet_client_bound.consume_args(args);
-        }
-
-        if !is_created
-        {
-            self.base_mut().remove_child(enet_client_node);
-            return false;
-        }
-
-        self.network_transport_type = NetworkTransport::ENetClient;
-        self.network_node = Some(enet_client_node.upcast());
-        return true;
-    }
 }
