6a775b2c82eab2efc7bead259af1bc7fc25715f9
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-11-27 00:48:57 +0100

    Minimum string length validator

---
+++ rust-src/src/ui/validators/min_validator.rs
@@ -0,0 +1,46 @@
+use godot::prelude::*;
+use godot::classes::{Resource, IResource};
+use crate::ui::validators::validators_holder::{ValidateResult, ValidatorConstruct, UIValueValidator};
+
+#[derive(GodotClass)]
+#[class(base = Resource, init)]
+struct MinValidatorResource
+{
+    base: Base<Resource>,
+    #[export]
+    min_characters: i64,
+}
+
+impl ValidatorConstruct for MinValidatorResource
+{
+    fn construct_validator(&self) -> Box<dyn UIValueValidator>
+    {
+        Box::new(MinValidator::new(self.min_characters.clone()))
+    }
+}
+
+struct MinValidator
+{
+    min_characters: i64,
+}
+
+
+impl MinValidator
+{
+    fn new(min_characters: i64) -> Self
+    {
+        Self
+        {
+            min_characters,
+        }
+    }
+}
+
+impl UIValueValidator for MinValidator
+{
+    fn validate_value(&self, value: &GString) -> ValidateResult
+    {
+        if value.len() > self.min_characters as usize { return ValidateResult::Success; }
+        ValidateResult::Failed
+    }
+}
