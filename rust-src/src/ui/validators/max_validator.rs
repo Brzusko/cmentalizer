809f662511f05d9fd05c5d505685387615a8282a
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-11-27 00:56:51 +0100

    Max string length validator

---
+++ rust-src/src/ui/validators/max_validator.rs
@@ -0,0 +1,50 @@
+use godot::prelude::*;
+use godot::classes::{Resource, IResource};
+use crate::ui::validators::validators_holder::{UIValueValidator, ValidateResult, ValidatorConstruct};
+
+#[derive(GodotClass)]
+#[class(base = Resource, init)]
+struct MaxValidatorResource
+{
+    base: Base<Resource>,
+    #[export]
+    max_characters: i64,
+    #[export]
+    revert: bool,
+}
+
+impl ValidatorConstruct for MaxValidatorResource
+{
+    fn construct_validator(&self) -> Box<dyn UIValueValidator>
+    {
+        Box::new(MaxValidator::new(self.max_characters.clone(), self.revert.clone()))
+    }
+}
+
+struct MaxValidator
+{
+    max_characters: i64,
+    revert: bool,
+}
+
+
+impl MaxValidator
+{
+    fn new(max_characters: i64, revert: bool) -> Self
+    {
+        Self
+        {
+            max_characters,
+            revert,
+        }
+    }
+}
+
+impl UIValueValidator for MaxValidator
+{
+    fn validate_value(&self, value: &GString) -> ValidateResult
+    {
+        if value.len() >= self.max_characters as usize { return ValidateResult::Success; }
+        if self.revert { ValidateResult::Revert} else { ValidateResult::Failed }
+    }
+}
--- rust-src/src/ui/validators/mod.rs
+++ rust-src/src/ui/validators/mod.rs
@@ -1,3 +1,4 @@
 pub (crate) mod validators_holder;
 pub (crate) mod regex_validator;
 pub (crate) mod min_validator;
+pub (crate) mod max_validator;
