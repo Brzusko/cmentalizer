80f91f2bc02ad4c8648788900cffbc199fda6e81
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-05 00:11:48 +0100

    Validators refactor done

--- rust-src/src/ui/validators/validator_consumers/line_edit_validator_consumer.rs
+++ rust-src/src/ui/validators/validator_consumers/line_edit_validator_consumer.rs
@@ -2,6 +2,8 @@ use std::fmt::Debug;
 use std::ops::{Deref, DerefMut};
 use godot::prelude::*;
 use godot::classes::{INode, LineEdit, Node};
+use log::error;
+use crate::ui::validators::ValidationResponse;
 use crate::ui::validators::validators_holder::{ValidatorsHolder};
 
 #[derive(GodotClass)]
@@ -13,6 +15,7 @@ pub(crate) struct LineEditValidatorConsumer
     validators_holder: Option<Gd<ValidatorsHolder>>,
     #[export]
     line_edit: Option<Gd<LineEdit>>,
+    on_text_changed_callable: Option<Callable>,
     cached_value: GString,
     caret_column: i32,
 }
@@ -23,8 +26,17 @@ impl INode for LineEditValidatorConsumer
     fn ready(&mut self)
     {
         if self.validators_holder.is_none() || self.line_edit.is_none() { return; }
-        
-        let validators_holder = self.validators_holder.as_mut().unwrap();
+
+        {
+            let validators_holder = self.validators_holder.as_mut().unwrap();
+            validators_holder.bind_mut().setup_validators();
+        }
+
+        {
+            let callable = self.base().callable("on_text_changed");
+            self.line_edit.as_mut().unwrap().connect("text_changed", &callable);
+            self.on_text_changed_callable = Some(callable);
+        }
     }
 }
 
@@ -34,5 +46,44 @@ impl LineEditValidatorConsumer
     #[func]
     pub fn on_text_changed(&mut self, new_text: GString)
     {
+        if self.validators_holder.is_none() { return; }
+        let validation_result;
+        {
+            let mut validators_holder = self.validators_holder.as_mut().unwrap().bind_mut();
+            validation_result = validators_holder.validate(&new_text);
+        }
+
+        match validation_result
+        {
+            Ok(()) => {}
+            Err(errors) => {
+                for error in errors.iter()
+                {
+                    match error {
+                        // For now, ignore pass error
+                        ValidationResponse::Pass(_) => {}
+                        ValidationResponse::Revert(_) => {
+                            self.revert();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        self.pass(new_text);
+    }
+
+    fn pass(&mut self, text: GString)
+    {
+        self.cached_value = text;
+        self.caret_column = self.line_edit.as_ref().unwrap().get_caret_column();
+    }
+
+    fn revert(&mut self)
+    {
+        let line_edit = self.line_edit.as_mut().unwrap();
+        line_edit.set_text(&self.cached_value);
+        line_edit.set_caret_column(self.caret_column);
     }
 }
