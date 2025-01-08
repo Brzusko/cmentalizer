5761a0115342214cd7c10470ddc4ad67d2360663
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-09 00:28:36 +0100

    Disable multi_runner editor plugin

--- rust-src/src/addons/multi_client_runner/main_multi_client.rs
+++ rust-src/src/addons/multi_client_runner/main_multi_client.rs
@@ -1,43 +1,43 @@
-use godot::prelude::*;
 use godot::classes::{Control, EditorPlugin, IEditorPlugin};
+use godot::prelude::*;
 
 static CONTROL_SCENE_PATH: &str = "res://addons/multi_client/scenes/MultiClientControl.tscn";
 
-#[derive(GodotClass)]
-#[class(base = EditorPlugin, init, tool)]
-struct MultiClientRunnerMain
-{
-    base: Base<EditorPlugin>,
-    main_control: Option<Gd<Control>>
-}
+// #[derive(GodotClass)]
+// #[class(base = EditorPlugin, init, tool)]
+// struct MultiClientRunnerMain
+// {
+//     base: Base<EditorPlugin>,
+//     main_control: Option<Gd<Control>>
+// }
 
-#[godot_api]
-impl IEditorPlugin for MultiClientRunnerMain
-{
-    fn enter_tree(&mut self) {
-        let packed_scene_result = try_load::<PackedScene>(&GString::from(CONTROL_SCENE_PATH));
-        let instance: Option<Gd<Control>>;
-        
-        match packed_scene_result
-        {
-            Ok(scene) => { instance = scene.try_instantiate_as::<Control>(); }
-            Err(_) => { godot_print!("Could not create multi-client-window"); return; }
-        }
-        
-        let control = instance.unwrap();
-        self.base_mut().add_control_to_bottom_panel(&control, &GString::from("Multi Client"));
-        
-        self.main_control = Some(control);
-    }
+// #[godot_api]
+// impl IEditorPlugin for MultiClientRunnerMain
+// {
+//     fn enter_tree(&mut self) {
+//         let packed_scene_result = try_load::<PackedScene>(&GString::from(CONTROL_SCENE_PATH));
+//         let instance: Option<Gd<Control>>;
 
-    fn exit_tree(&mut self) {
-        if self.main_control.is_none()
-        {
-            return;
-        }
-        
-        let control = self.main_control.take().unwrap();
-        self.base_mut().remove_control_from_bottom_panel(&control);
-        control.free();
-    }
-}
+//         match packed_scene_result
+//         {
+//             Ok(scene) => { instance = scene.try_instantiate_as::<Control>(); }
+//             Err(_) => { godot_print!("Could not create multi-client-window"); return; }
+//         }
+
+//         let control = instance.unwrap();
+//         self.base_mut().add_control_to_bottom_panel(&control, &GString::from("Multi Client"));
+
+//         self.main_control = Some(control);
+//     }
+
+//     fn exit_tree(&mut self) {
+//         if self.main_control.is_none()
+//         {
+//             return;
+//         }
+
+//         let control = self.main_control.take().unwrap();
+//         self.base_mut().remove_control_from_bottom_panel(&control);
+//         control.free();
+//     }
+// }
