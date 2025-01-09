295cb758853b23639c04d474ebfbf5bad24cf7b1
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-09 02:03:40 +0100

    WIP TestGameMode implementation

--- rust-src/src/gameframework/mod.rs
+++ rust-src/src/gameframework/mod.rs
@@ -1,16 +1,24 @@
+use godot::builtin::GString;
 use godot::classes::{Camera2D, Node2D};
 use godot::prelude::{Callable, DynGd, Gd, Vector2};
 
 trait Player {
     fn get_controlled_entity(&self) -> DynGd<Node2D, dyn ControlledEntity>;
 }
+
 trait ControlledEntity {
     fn apply_vertical_input(&mut self, input: Vector2);
     fn apply_aim_offset(&mut self, aim_offset: Vector2);
     fn override_state_basic(&mut self, new_position: Vector2, new_rotation: f64);
 }
+
 trait PlayerSpawner {
     fn spawn_player(&mut self, parent: Gd<Node2D>, event_handler: Callable) -> anyhow::Result<()>;
+}
+
+trait GameMode {
+    // maybe generic data?
+    fn initialize(&mut self) -> anyhow::Result<(), GString>;
 }
 
 pub(crate) mod simple_player_spawner;
--- rust-src/src/gameframework/test_gamemode.rs
+++ rust-src/src/gameframework/test_gamemode.rs
@@ -1,7 +1,57 @@
+use crate::gameframework::{Player, PlayerSpawner};
+use anyhow::anyhow;
+use godot::classes::{INode, Node};
 use godot::prelude::*;
+
+use super::GameMode;
 
 #[derive(GodotClass)]
 #[class(base = Node, init)]
 struct TestGameMode {
     base: Base<Node>,
+    #[export]
+    spawner_reference: Option<Gd<Node>>,
+    spawner_ptr: Option<DynGd<Node, dyn PlayerSpawner>>,
+    player_ptr: Option<DynGd<Node2D, dyn Player>>,
+}
+
+#[godot_api]
+impl INode for TestGameMode {
+    fn ready(&mut self) {
+        // TODO initialize should be invoked outside GameMode context
+        if let Err(error) = self.initialize() {
+            godot_error!("{:?}", error);
+            return;
+        }
+    }
+}
+
+#[godot_dyn]
+impl GameMode for TestGameMode {
+    fn initialize(&mut self) -> anyhow::Result<(), GString> {
+        if self.spawner_ptr.is_none() {
+            return Err(GString::from(
+                "GameMode - player spawner reference is missing!",
+            ));
+        }
+
+        {
+            let spawner_cast = self
+                .spawner_reference
+                .as_mut()
+                .unwrap()
+                .to_variant()
+                .try_to::<DynGd<Node, dyn PlayerSpawner>>();
+
+            if let Err(_) = spawner_cast {
+                return Err(GString::from(
+                    "GameMode - could not grab spawner interface pointer.",
+                ));
+            }
+
+            self.spawner_ptr = Some(spawner_cast.unwrap());
+        }
+
+        Ok(())
+    }
 }
