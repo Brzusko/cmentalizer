4836ab183c84ec79265c8b34945fee30d220c99c
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-10 01:27:35 +0100

    Spawning player obj in test game mode

--- rust-src/src/gameframework/test_gamemode.rs
+++ rust-src/src/gameframework/test_gamemode.rs
@@ -11,6 +11,8 @@ struct TestGameMode {
     base: Base<Node>,
     #[export]
     spawner_reference: Option<Gd<Node>>,
+    #[export]
+    world: Option<Gd<Node2D>>,
     spawner_ptr: Option<DynGd<Node, dyn PlayerSpawner>>,
     player_ptr: Option<DynGd<Node2D, dyn Player>>,
 }
@@ -23,12 +25,30 @@ impl INode for TestGameMode {
             godot_error!("{:?}", error);
             return;
         }
+
+        // world ref is checked in self.initialize
+        let world_ptr = self.world.as_mut().unwrap();
+        // spawner ref is checked in self.initialize
+        let spawner_ptr = self.spawner_ptr.as_mut().unwrap();
+
+        let player_spawn_result = spawner_ptr.dyn_bind_mut().spawn_player(world_ptr.clone());
+
+        if let Err(_) = player_spawn_result {
+            godot_error!("GameMode - could not spawn player!");
+            return;
+        }
+
+        self.player_ptr = Some(player_spawn_result.unwrap());
     }
 }
 
 #[godot_dyn]
 impl GameMode for TestGameMode {
     fn initialize(&mut self) -> anyhow::Result<(), GString> {
+        if self.world.is_none() {
+            return Err(GString::from("GameMode - world reference is missing!"));
+        }
+
         if self.spawner_ptr.is_none() {
             return Err(GString::from(
                 "GameMode - player spawner reference is missing!",
